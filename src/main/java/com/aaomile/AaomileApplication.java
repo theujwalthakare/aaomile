39dbfb7e89d82f2bba52c75c8f40f7760a911035
thaka <thakre2829@gmail.com>
2024-12-26 23:24:49 +0530

    new changes

---
+++ src/main/java/com/aaomile/AaomileApplication.java
@@ -0,0 +1,13 @@
+package com.aaomile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaomileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaomileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaomile/AaomileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaomile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaomileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
