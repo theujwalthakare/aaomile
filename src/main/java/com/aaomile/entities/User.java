f302237536ddf0717f28018017ef4e19b02bb94e
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-01-02 20:43:33 +0530

    user table

---
+++ src/main/java/com/aaomile/entities/User.java
@@ -0,0 +1,35 @@
+package com.aaomile.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import jakarta.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(nullable = false)
+    private String id;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String address;
+}
