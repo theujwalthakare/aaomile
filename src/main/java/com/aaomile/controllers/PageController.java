025b9a18f826f0936db24d990eb8a5ab274cd7e2
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-01-02 20:41:54 +0530

    controller for after login

--- src/main/java/com/aaomile/controllers/PageController.java
+++ src/main/java/com/aaomile/controllers/PageController.java
@@ -1,12 +1,18 @@
 
 package com.aaomile.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import UserForms.Login;
+import com.aaomile.entities.Login;
+import com.aaomile.repository.UserRepository;
+
 import ch.qos.logback.core.model.Model;
 
 
@@ -44,15 +50,27 @@ public class PageController {
         return "Events";
     }
 
+    // @RequestMapping(value= "/after_login", method=RequestMethod.POST)
+    // public String processLogin(@ModelAttribute Login login) {
+    //     System.out.println(login);
+    //     System.out.println("Login Button Hit");
+    //     return"after_login";
+    // }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // @PostMapping("/login")
     @RequestMapping(value= "/after_login", method=RequestMethod.POST)
-    public String processLogin(@ModelAttribute Login login) {
-        System.out.println(login);
-        System.out.println("Login Button Hit");
-        return"after_login";
+    public String login(@RequestParam String id, @RequestParam String password, Model model) {
+        Optional<Login> user = userRepository.findByIdAndPassword(id, password);
+        if (user.isPresent()) {
+            return "after_login"; // Redirect to home page
+        } else {
+            // model.addAttribute("error", "Invalid ID or password");
+            return "login";
+        }
     }
 
-    //route
-      
-     
-    //services
 }
+
