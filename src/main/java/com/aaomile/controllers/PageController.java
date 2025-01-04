324b7724d679477159809a7a18d0ed31b0182366
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-01-04 19:35:55 +0530

    added registertion controller

--- src/main/java/com/aaomile/controllers/PageController.java
+++ src/main/java/com/aaomile/controllers/PageController.java
@@ -1,19 +1,21 @@
-
 package com.aaomile.controllers;
 
 import java.util.Optional;
-
+import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.aaomile.entities.Login;
-import com.aaomile.repository.UserRepository;
-
-import ch.qos.logback.core.model.Model;
+import com.aaomile.entities.User;
+import com.aaomile.forms.UserReg;
+import com.aaomile.repository.LoginRepository;
+import com.aaomile.service.UserService;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 @Controller
@@ -22,10 +24,6 @@ public class PageController {
     @RequestMapping("/")
     public String index(Model model) {
         return "home";
-    }
-    @RequestMapping("/signup")
-    public String sigup(Model model) {
-        return "signup";
     }
     @RequestMapping("/about")
     public String about(Model model) {
@@ -50,27 +48,56 @@ public class PageController {
         return "Events";
     }
 
-    // @RequestMapping(value= "/after_login", method=RequestMethod.POST)
-    // public String processLogin(@ModelAttribute Login login) {
-    //     System.out.println(login);
-    //     System.out.println("Login Button Hit");
-    //     return"after_login";
-    // }
-
+    @RequestMapping("/after_login")
+    public String afterLogin(Model model) {
+        return "after_login";
+    }
+    
     @Autowired
-    private UserRepository userRepository;
-
-    // @PostMapping("/login")
-    @RequestMapping(value= "/after_login", method=RequestMethod.POST)
+    private LoginRepository loginRepository;
+    
+    // @RequestMapping(value= "/after_login", method=RequestMethod.POST)
+    @PostMapping("/after_login")
     public String login(@RequestParam String id, @RequestParam String password, Model model) {
-        Optional<Login> user = userRepository.findByIdAndPassword(id, password);
+        Optional<Login> user = loginRepository.findByIdAndPassword(id, password);
+        System.out.println("Login Button Hit");
         if (user.isPresent()) {
-            return "after_login"; // Redirect to home page
+            return "after_login"; 
+            
         } else {
-            // model.addAttribute("error", "Invalid ID or password");
+            model.addAttribute("error", "Invalid ID or password");
             return "login";
         }
     }
+    
+    @Autowired
+    private UserService userService;
+    
+    @RequestMapping("/signup")
+    public String sigup(Model model) {
+        UserReg userReg = new UserReg();
+        model.addAttribute("userReg", userReg);
+        return "signup";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String signup(@ModelAttribute UserReg userReg) {
+
+        User user = User.builder()
+        .firstName(userReg.getFirstName())
+        .lastName(userReg.getLastName())
+        .email(userReg.getEmail())
+        .password(userReg.getPassword())
+        .build();
+
+        userService.createUser(user);
+
+        System.out.println("created user");
+        System.out.println(userReg);
+        System.out.println("Processing User register");
+
+
+        return "redirect:/login";
+    }
 
 }
-
