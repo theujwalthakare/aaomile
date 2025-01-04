a46801b8c694c66cf573cf2c6bade88d29387ca8
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-01-04 21:21:05 +0530

    exists By Email And Password method

--- src/main/java/com/aaomile/service/UserService.java
+++ src/main/java/com/aaomile/service/UserService.java
@@ -1,12 +1,18 @@
 package com.aaomile.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.aaomile.entities.User;
+
 
 @Service
 public interface UserService {
 
     User createUser(User user);
+
+    // Optional<User> getUserByEmailandPassword(String email, String password);
+    boolean existsByEmailAndPassword(String email, String password);
 
 }
