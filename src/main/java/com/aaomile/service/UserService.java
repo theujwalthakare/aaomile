1889c4ad0af9c26ba177a9afa9c5084c873c7f46
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-01-04 21:21:33 +0530

    exists By Email And Password method

--- src/main/java/com/aaomile/service/UserService.java
+++ src/main/java/com/aaomile/service/UserService.java
@@ -1,7 +1,5 @@
 package com.aaomile.service;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
 
 import com.aaomile.entities.User;
@@ -10,9 +8,8 @@ import com.aaomile.entities.User;
 @Service
 public interface UserService {
 
-    User createUser(User user);
+    User createUser(User user); //to insert vales in user table
 
-    // Optional<User> getUserByEmailandPassword(String email, String password);
-    boolean existsByEmailAndPassword(String email, String password);
+    boolean existsByEmailAndPassword(String email, String password);  // to check the email and password for login
 
 }
