fc878ae18a7d13d6005ab70da6740c79609c62c9
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-01-10 02:16:19 +0530

    login techiques

--- src/main/java/com/aaomile/config/OAuthAthenticationSuccessHandler.java
+++ src/main/java/com/aaomile/config/OAuthAthenticationSuccessHandler.java
@@ -6,15 +6,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import com.aaomile.entities.User;
+import com.aaomile.entities.Providers;
 import com.aaomile.repository.UserRepo;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.var;
 
 @Component
 public class OAuthAthenticationSuccessHandler implements AuthenticationSuccessHandler {
@@ -29,37 +32,63 @@ public class OAuthAthenticationSuccessHandler implements AuthenticationSuccessHa
             HttpServletRequest request, 
             HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
+
+
             logger.info("OAuthAthenticationSuccessHandler");
 
-            DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
-            logger.info(user.getName());
-            user.getAttributes().forEach((key,value)->{
-                logger.info("{} => {}",key,value);
-            });
-            logger.info(user.getAuthorities().toString());
+            var oauth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+            
+            String authorizedClientRegistrationId = oauth2AuthenticationToken.getAuthorizedClientRegistrationId();
+            
+            logger.info(authorizedClientRegistrationId);
 
-            String firstName = user.getAttribute("given_name").toString();
-            String lastName = user.getAttribute("family_name").toString();
-            String email = user.getAttribute("email").toString();
-            // String name = user.getAttribute("name").toString();
-            String picture = user.getAttribute("picture").toString();
+            var oauthUser = (DefaultOAuth2User)authentication.getPrincipal();
+            
+            oauthUser.getAttributes().forEach((key, value) -> {
+                logger.info(key + " => " + value);
+            });
+
+            User user = new User();
+            
+            if(authorizedClientRegistrationId.equalsIgnoreCase("google")){
+                
+                String firstName = oauthUser.getAttribute("given_name");
+                String lastName = oauthUser.getAttribute("family_name");
+                String email = oauthUser.getAttribute("email");
+                String picture = oauthUser.getAttribute("picture").toString();
+                
+                user.setEmail(email);
+                user.setFirstName(firstName);
+                user.setLastName(lastName);
+                user.setFullName(firstName+" "+lastName);
+                user.setEmail(email);
+                user.setEmailVerified(true);
+                user.setProfilePic(picture);
+                user.setPassword("defaultPassword");
+                user.setProvider("GOOGLE");
+
+            }
+            else{
+                logger.info("OAuthAthenticationSuccessHandler: Unknown Provider");
+            }
+
+            // logger.info(user.getAuthorities().toString());
+
 
             //create user and save in databse
-            User user1 = new User();
+            // User user1 = new User();
 
-            user1.setFirstName(firstName);
-            user1.setLastName(lastName);
-            user1.setFullName(firstName+" "+lastName);
-            user1.setEmail(email);
-            user1.setProfilePic(picture);
-
-            user1.setPassword("defaultPassword");
-            user1.setEmailVerified(true);
+            // user1.setFirstName(firstName);
+            // user1.setLastName(lastName);
+            // user1.setFullName(firstName+" "+lastName);
+            // user1.setEmail(email);
+            // user1.setProfilePic(picture);
+            // user1.setPassword("defaultPassword");
 
 
-            User user2 = userRepo.findByEmail(email).orElse(null);
+            User user2 = userRepo.findByEmail(user.getEmail()).orElse(null);
             if(user2 == null){
-                userRepo.save(user1);
+                userRepo.save(user);
                 logger.info("User saved");
             }
             else{
