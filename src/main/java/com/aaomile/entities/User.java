fe2f7c948a6110693c07beff80be8b93d0d02a55
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-01-10 02:17:35 +0530

    removed unsed imports

--- src/main/java/com/aaomile/entities/User.java
+++ src/main/java/com/aaomile/entities/User.java
@@ -11,8 +11,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
