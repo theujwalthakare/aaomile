94ccbfd35cf70aa200aefcd0998fb11862dba19e
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-01-10 02:11:50 +0530

    adding model and authentication

--- src/main/java/com/aaomile/controllers/UserController.java
+++ src/main/java/com/aaomile/controllers/UserController.java
@@ -1,22 +1,32 @@
 package com.aaomile.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aaomile.service.UserService;
 
 @Controller
 
 @RequestMapping("/user")
 public class UserController {
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/after_login")
-    public String after_login() {
+    public String after_login(Model model, Authentication authentication) {
         return "user/after_login";
     }
 
-    @RequestMapping("/CreateEvent")    //user/create event
-    public String creatEvent(Model model) {
+    @RequestMapping("/CreateEvent")    //       user/create event
+    public String createEvent(Model model) {
         return "user/CreateEvent";
     }
 
@@ -29,7 +39,6 @@ public class UserController {
     // public String book(Model model) {
     //     return "user/book";
     // }
-
     // user/dashboard
     // user/edit profile
     // user/bookevent 
