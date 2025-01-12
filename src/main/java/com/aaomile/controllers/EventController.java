1517e3e7c16460237fec9cfee2ee4e04b9b26e64
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-01-12 11:05:49 +0530

    controller to add events

--- src/main/java/com/aaomile/controllers/EventController.java
+++ src/main/java/com/aaomile/controllers/EventController.java
@@ -1,7 +1,12 @@
 package com.aaomile.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.aaomile.entities.Event;
+import com.aaomile.forms.EventFrom;
 
 @Controller
 @RequestMapping("/user/event")
@@ -9,8 +14,18 @@ public class EventController {
 
 
     @RequestMapping("/create")
-    public String addEventView(){
+    public String addEventView(Model model){
+
+        EventFrom eventFrom = new EventFrom();
+
+        model.addAttribute("eventForm", eventFrom);
         return "user/eventFormApplication";
+    }
+
+    @RequestMapping(value = "/add", method =  RequestMethod.POST)
+    public String createEvent(Event event){
+
+        return "";
     }
 
 }
