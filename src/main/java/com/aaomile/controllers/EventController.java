53dddb58c039a7d20401e258da9bbe54ec7aab23
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-01-14 01:11:44 +0530

    Cloudinary programs for Banner

--- src/main/java/com/aaomile/controllers/EventController.java
+++ src/main/java/com/aaomile/controllers/EventController.java
@@ -1,5 +1,9 @@
 package com.aaomile.controllers;
 
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -13,14 +17,20 @@ import com.aaomile.entities.Event;
 import com.aaomile.entities.User;
 import com.aaomile.forms.EventFrom;
 import com.aaomile.helper.Helper;
+import com.aaomile.helper.Message;
+import com.aaomile.helper.MessageType;
 import com.aaomile.service.EventService;
+import com.aaomile.service.ImageService;
 import com.aaomile.service.UserService;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/user/event")
 public class EventController {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private EventService eventService;
@@ -28,26 +38,31 @@ public class EventController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ImageService imageService;
+
     @RequestMapping("/create")
     public String addEventView(Model model){
+        Logger logger = LoggerFactory.getLogger(this.getClass());
 
         EventFrom eventFrom = new EventFrom();
         model.addAttribute("eventForm", eventFrom);
+        
         return "user/eventFormApplication";
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String saveEvent(@Valid @ModelAttribute EventFrom eventFrom,BindingResult result ,Authentication authentication){
-
-        String username = Helper.getEmailOfLoggedInUser(authentication);
-        User user = userService.getUserByEmail(username);
+    public String saveEvent(@Valid @ModelAttribute EventFrom eventFrom,BindingResult result ,Authentication authentication, HttpSession session){
 
         String email = Helper.getEmailOfLoggedInUser(authentication);
+        User user = userService.getUserByEmail(email);
+
+        // String email = Helper.getEmailOfLoggedInUser(authentication);
 // 
         // User uname = userService.getUserByEmail(email);
 
 
-        //validation
+        // //validation
         if(result.hasErrors()){
             
             return "user/eventFormApplication";
@@ -55,13 +70,20 @@ public class EventController {
 
         //process image
 
+        // String fileName = UUID.randomUUID().toString();   //this creates a random 128 bits ID 
+        String bannerName = eventFrom.getEventName();        // this takes the event name as id for to store at cloudinary
+        //banner upload programm
+        String bannerURL =  imageService.uploadImage(eventFrom.getEventBanner(),bannerName);
+
+
         Event event = new Event();
         event.setEventName(eventFrom.getEventName());
         event.setEventDiscription(eventFrom.getEventDiscription());
         event.setEventGenre(eventFrom.getEventGenre());
         event.setEventType(eventFrom.getEventType());
-        // event.setEventBanner(eventFrom.getEventBanner());
-        event.setEventDate(eventFrom.getEventDate());   
+        event.setEventBanner(bannerURL);  //banner url to DB
+        event.setCloudBannerId(bannerName); //random generated file
+        event.setEventDate(eventFrom.getEventDate());
         event.setEventTime(eventFrom.getEventTime());
         event.setEventVenue(eventFrom.getEventVenue());
         event.setEventAddress(eventFrom.getEventAddress());
@@ -71,13 +93,23 @@ public class EventController {
         event.setTicketType(eventFrom.getTicketType());
         event.setTicketPrice(eventFrom.getTicketPrice());
         event.setSeates(eventFrom.getSeates());
-        // event.setEventDuration(eventFrom.getHours()+"hr"+eventFrom.getMinutes()+"min"+eventFrom.getSeconds()+"sec");
         event.setEventDuration(eventFrom.getHours()+":"+eventFrom.getMinutes()+":"+eventFrom.getSeconds());
         event.setUser(user);
         event.setCreatorEmail(email); 
 
         eventService.save(event);
-        System.out.println(eventFrom);
+        logger.info("EVENT CREATTION STARTED");
+        logger.info("EVENT CREATED AND SAVED");
+        logger.info("CREATOR USER ID => {}",user);
+        logger.info("CREATOR USER EMAIL => {}",email);
+        logger.info(eventFrom.toString());
+        logger.info("EVENT CREATTION ENDED");
+
+
+        session.setAttribute("message", Message.builder()
+        .content("Event Created Successfully ")
+        .type(MessageType.green)
+        .build());
         return "redirect:/user/event/create";
 
     }
