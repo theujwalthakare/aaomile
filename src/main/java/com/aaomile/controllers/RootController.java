ca0cc6a8a07db5e9378d79d2c3bb79c3160da3b2
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2025-01-14 23:44:35 +0530

    model attribute to display event by genre

--- src/main/java/com/aaomile/controllers/RootController.java
+++ src/main/java/com/aaomile/controllers/RootController.java
@@ -1,6 +1,8 @@
 package com.aaomile.controllers;
 
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.security.core.Authentication;
+import com.aaomile.entities.Event;
 import com.aaomile.entities.User;
 import com.aaomile.helper.Helper;
+import com.aaomile.service.EventService;
 import com.aaomile.service.UserService;
 
 @ControllerAdvice
@@ -19,6 +23,9 @@ public class RootController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired 
+    private EventService eventService;
     
     @ModelAttribute
     public void addLoggedInUserInformation(Model model, Authentication authentication) {
@@ -38,5 +45,62 @@ public class RootController {
         logger.info("PROFILE PIC =>"+user.getProfilePic());
         model.addAttribute("loggedInUser", user);
     }
+
+    @ModelAttribute
+    public void eventByComedy(Model model){
+        String genre = "comedy";
+
+        List<Event> events = eventService.getByGenre(genre);
+        model.addAttribute("comedyEvent", events);
+        // return"comedy";
+    }
+
+    @ModelAttribute
+    public void eventByMusic(Model model){
+        String genre = "music";
+
+        List<Event> events = eventService.getByGenre(genre);
+        model.addAttribute("musicEvent", events);
+        // return"comedy";
+    }
+
+    @ModelAttribute
+    public void eventByWorkshop(Model model){
+        String genre = "workshop";
+
+        List<Event> events = eventService.getByGenre(genre);
+        model.addAttribute("workshopEvent", events);
+        // return"comedy";
+    }
+
+    @ModelAttribute
+    public void eventByExhibition(Model model){
+        String genre = "exhibition";
+
+        List<Event> events = eventService.getByGenre(genre);
+        model.addAttribute("exhibitionEvent", events);
+        // return"comedy";
+    }
+
+    @ModelAttribute
+    public void eventByTheater(Model model){
+        String genre = "theater";
+
+        List<Event> events = eventService.getByGenre(genre);
+        model.addAttribute("theaterEvent", events);
+        // return"comedy";
+    }
+
+    @ModelAttribute
+    public void eventByHackathon(Model model){
+        String genre = "hackathon";
+
+        List<Event> events = eventService.getByGenre(genre);
+        model.addAttribute("hackathonEvent", events);
+        // return"comedy";
+    }
+
+
+
 
 }
